ef71bebc893977d5bda24d034c81c8ffc34a4fd2
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2025-03-18 13:37:54 -0400

    Fix incorrect python packaging
    As a result it needs to start a python venv while in nix shell it will work.

--- requirements.nix
+++
@@ -1,8 +0,0 @@
-{ ps }: [
-    ps.loguru
-    ps.pyserial
-    ps.tqdm
-    ps.cffi
-    ps.pycparser
-    ps.pynacl
-]
--- shell.nix
+++ shell.nix
@@ -18,13 +18,9 @@
   , unzip ? pkgs.unzip
 }:
 
-let 
-   pythonEnv = pkgs.python3.withPackages (ps: with ps; (import ./requirements.nix {inherit ps;}));
 
-in
 pkgs.mkShell {
 
-  packages = [ pythonEnv ];
   buildInputs = [
     pkgs.gnumake
     pkgs.python311

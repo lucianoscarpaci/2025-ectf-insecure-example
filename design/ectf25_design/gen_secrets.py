b5defd7aa59efa686785793d9f67ad2f5980914f
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2025-03-24 23:02:36 -0400

    Add wolfSSL ed25519
    -pynacl
    -pynacl signatures
    -old text
    +wolfSSL ed25519 signing
    +wolfSSL ed25519 verification
    +wolfSSL random

--- design/ectf25_design/gen_secrets.py
+++ design/ectf25_design/gen_secrets.py
@@ -1,57 +1,38 @@
 """
-Author: Luciano Scarpaci Copyright 2025
-Author: Ben Janis
+Author: Luciano Scarpaci
 Date: 2025
-
-This source file is part of an example system for MITRE's 2025 Embedded System CTF
-(eCTF). This code is being provided only for educational purposes for the 2025 MITRE
-eCTF competition, and may not meet MITRE standards for quality. Use this code at your
-own risk!
-
-Copyright: Copyright (c) 2025 The MITRE Corporation
 """
 
 import argparse
 import json
-import secrets
-import hashlib
+from wolfcrypt.random import Random
+from wolfcrypt.ciphers import Ed25519Private
 import base64
-from nacl.signing import SigningKey
 from pathlib import Path
 from loguru import logger
 
 
 def gen_secrets(channels: list[int]) -> bytes:
-    """Generate the contents secrets file
 
-    This will be passed to the Encoder, ectf25_design.gen_subscription, and the build
-    process of the decoder
-
-    :param channels: List of channel numbers that will be valid in this deployment.
-        Channel 0 is the emergency broadcast, which will always be valid and will
-        NOT be included in this list
-
-    :returns: Contents of the secrets file
-    """
     try:
-        # Generate an 16-byte sk
-        sk = secrets.token_bytes(16)
-        # Generate a 16-byte iv
-        iv = secrets.token_bytes(16)
+        # Generate a 16-byte secret key (sk)
+        sk = Random().bytes(16)
+        # Generate a 16-byte initialization vector (iv)
+        iv = Random().bytes(16)
+        # Concatenate the sk and iv
+        sk_iv = sk + iv
         # Generate a new ED25519 signing key
-        signing_key = SigningKey.generate()
-        # Signs the nonce using the signing key
-        signed_message = signing_key.sign(sk + iv)
-        # define the public key
-        public_key = signing_key.verify_key
+        signing_key = Ed25519Private().make_key(size=32, rng=Random())
+        # Derive the public key from the signing key
+        public_key = signing_key.encode_key()[1]
         # Extract signature (ED25519) from signed message
-        signature = signed_message.signature
-        # result including channels, nonce, public key, and signature
+        signature = signing_key.sign(sk_iv)
+
         result = {
             "channels": channels,
             "sk": base64.b64encode(sk).decode("utf-8"),
             "iv": base64.b64encode(iv).decode("utf-8"),
-            "public_key": base64.b64encode(public_key.encode()).decode("utf-8"),
+            "public_key": base64.b64encode(public_key).decode("utf-8"),
             "signature": base64.b64encode(signature).decode("utf-8"),
         }
     except Exception as e:
@@ -97,12 +78,6 @@ def main():
 
     secrets, sk, iv, public_key, signature = gen_secrets(args.channels)
 
-    # Print the generated secrets for your own debugging
-    # Attackers will NOT have access to the output of this, but feel free to remove
-    #
-    # NOTE: Printing sensitive data is generally not good security practice
-    logger.debug(f"Generated secrets: {secrets}")
-
     # Open the file, erroring if the file exists unless the --force arg is provided
     with open(args.secrets_file, "wb" if args.force else "xb") as f:
         # Dump the secrets to the file
@@ -121,9 +96,6 @@ def main():
                 f.write(hex(iv[i]) + ", ")
             f.write(hex(iv[len(iv) - 1]) + "}\n")
             f.write("#endif\n")
-
-    # For your own debugging. Feel free to remove
-    logger.success(f"Wrote secrets to {str(args.secrets_file.absolute())}")
 
 
 if __name__ == "__main__":
